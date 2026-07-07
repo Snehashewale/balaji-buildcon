4a4f1c53001ce640d9c451795c216eec59287dbb
Sneha Shewale <shewalesneha21@gmail.com>
2026-07-07 10:30:53 +0530

    Add request logging

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    console.log(`${new Date().toISOString()} ${req.method} ${req.url}`);
+    next();
+});
 
 // Serve public folder
 app.use(express.static(path.join(__dirname, "public")));
